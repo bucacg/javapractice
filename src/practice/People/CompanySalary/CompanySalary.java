6b2c4a4012578fb532a8988a41b4ff223c7381eb
Bucacg <2759343358@qq.com>
2021-04-25 21:53:14 +0800

    向上转型练习

---
+++ src/practice/People/CompanySalary/CompanySalary.java
@@ -0,0 +1,60 @@
+package practice.People.CompanySalary;
+    abstract class Employee {
+        public abstract double earnings();
+    }
+    class YearWorker extends Employee {
+        public double earnings() {
+            return 12000;
+        }
+        //重写earnings()方法
+    }
+    class MonthWorker extends Employee {
+        public double earnings() {
+            return 12*2300 ;
+        }
+        //重写earnings()方法
+    }
+    class WeekWorker extends Employee {
+        public double earnings() {
+            return 52*780;
+        }
+        //重写earnings()方法。
+    }
+class dayWorker extends Employee {
+    public double earnings() {
+        return 100;
+    }
+    //重写earnings()方法
+}
+    class Company {
+        Employee[] employee;
+        double salaries = 0;
+        Company(Employee[] employee) {
+            this.employee = employee;
+        }
+        public double salariesPay() {
+            salaries = 0;
+            for (int i = 0; i < employee.length; i++) {
+                salaries=salaries+employee[i].earnings();
+            }
+            return salaries;
+        }
+    }
+    public class CompanySalary {
+        public static void main(String args[]) {
+            Employee [] employee=new Employee[29]; //公司有29名雇员
+            for(int i=0;i<employee.length;i++) {   //雇员简单地分成三类
+                if(i%3==0)
+                    employee[i]=new WeekWorker();
+                else if(i%3==1)
+                    employee[i]=new MonthWorker();
+                else if(i%3==2)
+                    employee[i]=new YearWorker();
+                else if(i==1)
+                    employee[i]=new YearWorker();
+            }
+            Company company=new Company(employee);
+            System.out.println("公司薪水总额:"+company.salariesPay()+"元");
+        }
+    }
+
